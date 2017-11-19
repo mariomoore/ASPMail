43aa79b8f1e7a0e20335d7748e338a2e6924c869
Mariusz Murawski <mariusz.murawski84@gmail.com>
2017-11-19 09:48:00 +0100

    Utworzenie modelu, kontrolera i widoków

---
+++ ASPMail/Controllers/ContactFormController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ASPMail.Models;
+
+namespace ASPMail.Controllers
+{
+    public class ContactFormController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: ContactForm
+        public ActionResult Index()
+        {
+            return View(db.ContactForms.ToList());
+        }
+
+        // GET: ContactForm/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ContactForm contactForm = db.ContactForms.Find(id);
+            if (contactForm == null)
+            {
+                return HttpNotFound();
+            }
+            return View(contactForm);
+        }
+
+        // GET: ContactForm/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ContactForm/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(ContactForm contactForm)
+        {
+            if (ModelState.IsValid)
+            {
+                db.ContactForms.Add(contactForm);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(contactForm);
+        }
+
+       
+
+        
+
+        // GET: ContactForm/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ContactForm contactForm = db.ContactForms.Find(id);
+            if (contactForm == null)
+            {
+                return HttpNotFound();
+            }
+            return View(contactForm);
+        }
+
+        // POST: ContactForm/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            ContactForm contactForm = db.ContactForms.Find(id);
+            db.ContactForms.Remove(contactForm);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+       
+    }
+}
---
+++ ASPMail/Models/ContactForm.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ASPMail.Models
+{
+    public class ContactForm
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+    }
+}
